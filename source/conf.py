ec169bf5e020ff6177ac8f6aecf5875c403cd1b7
Malpsh <15713902669@163.com>
2025-10-16 22:49:44 +0800

    fix: 修正 GitHub 仓库链接配置

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
-project = 'stCAMBL'
+project = 'stCAMBL_tutorial'
 copyright = '2025, AI4BREAD'
 author = 'Laiyi Fu, Wenkai Cui & Yifan Chen etc.'
  
